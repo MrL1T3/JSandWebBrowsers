90e41303bcc1f86220e3cf2e62e08fd93321db12
Emil Anton <emilbstn@gmail.com>
2022-11-05 23:21:05 -0700

    add testing script to html document to verify basic function idea

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,13 @@ function question(text) {
   let jsCount = 0;
   let pythonCount = 0;
   let cSharpCount = 0;
-}
+
+
+//  const btn = document.querySelector('btn')
+//  const testingSelector = document.getElementById('question1')
+//  btn.onClick = (event) => {
+//    event.preventDefault();
+//    alert(sb.selectedIndex);
+//  };
+//  document.getElementById("btn").addEventListener("onClick", function(){alert(sb.slectedIndex)});
+};
