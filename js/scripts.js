9bc1f1f79c326e1f771e10fe396a7fcd2bd0149f
Emil Anton <emilbstn@gmail.com>
2022-11-06 00:06:51 -0700

    update index.html and scripts.js with in progress functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
-function question(text) {
+window.onload=function(){
   let jsCount = 0;
   let pythonCount = 0;
   let cSharpCount = 0;
+  let answer = "";
+  let result = "";
 
 
-//  const btn = document.querySelector('btn')
-//  const testingSelector = document.getElementById('question1')
-//  btn.onClick = (event) => {
-//    event.preventDefault();
-//    alert(sb.selectedIndex);
-//  };
-//  document.getElementById("btn").addEventListener("onClick", function(){alert(sb.slectedIndex)});
+  const btn = document.querySelector('btn');
+  document.getElement('select').addEventListener
+  document.getElementById('btn').addEventListener('click', function(){alert(result);});
+  function questionOne(text) {
+    if (document.getElementById("questionOne").selectedIndex = "0")
+    jsCount ++;
+    else if (document.getElementById("questionOne").selectedIndex = "1")
+    pythonCount ++;
+    else
+    cSharpCount ++;
+    
+    if (jsCount > pythonCount || jsCount > cSharpCount)
+      answer = "JavaScript is your Language"
+    else if (pythonCount > jsCount || pythonCount > cSharpCount)
+      answer = "Python is your language"
+    else if (cSharpCount > jsCount || cSharpCount > pythonCount)
+      answer = "C# is your language"
+    else 
+      answer = "Try again with a little more thought, we didn't get enough specifics"
+    result = answer
+  };
 };
