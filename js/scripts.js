f6d9f03339a56044cecafe474f1c62f47c54be58
Emil Anton <emilbstn@gmail.com>
2022-11-29 12:19:45 -0800

    add new event listener to first select element

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,47 @@ window.onload=function(){
   let answer = "";
   let result = "";
 
+  function questionOne() {
+      if (document.getElementById("questionOne").selectedIndex = "0")
+      jsCount ++;
+      else if (document.getElementById("questionOne").selectedIndex = "1")
+      pythonCount ++;
+      else
+      cSharpCount ++;
+      
+      if (jsCount > pythonCount || jsCount > cSharpCount)
+        answer = "JavaScript is your Language"
+      else if (pythonCount > jsCount || pythonCount > cSharpCount)
+        answer = "Python is your language"
+      else if (cSharpCount > jsCount || cSharpCount > pythonCount)
+        answer = "C# is your language"
+      else 
+        answer = "Try again with a little more thought, we didn't get enough specifics"
+      result = answer
+    };
 
-  const btn = document.querySelector('btn');
-  document.getElement('select').addEventListener
-  document.getElementById('btn').addEventListener('click', function(){alert(result);});
-  function questionOne(text) {
-    if (document.getElementById("questionOne").selectedIndex = "0")
-    jsCount ++;
-    else if (document.getElementById("questionOne").selectedIndex = "1")
-    pythonCount ++;
-    else
-    cSharpCount ++;
+  let a = document.getElementById('questionOne');
+  a.addEventListener('change', function() {console.log(this.value);}, false);
+  document.getElementById('btn').addEventListener('click', resultCounter);
+};  
+  // const btn = document.querySelector('btn');
+  // document.querySelectorAll('select').addEventListener('click', function(){alert(result);});
+  // document.getElementById('btn').addEventListener('click', function(){(result);});
+  // function questionOne() {
+  //   if (document.getElementById("questionOne").selectedIndex = "0")
+  //   jsCount ++;
+  //   else if (document.getElementById("questionOne").selectedIndex = "1")
+  //   pythonCount ++;
+  //   else
+  //   cSharpCount ++;
     
-    if (jsCount > pythonCount || jsCount > cSharpCount)
-      answer = "JavaScript is your Language"
-    else if (pythonCount > jsCount || pythonCount > cSharpCount)
-      answer = "Python is your language"
-    else if (cSharpCount > jsCount || cSharpCount > pythonCount)
-      answer = "C# is your language"
-    else 
-      answer = "Try again with a little more thought, we didn't get enough specifics"
-    result = answer
-  };
-};
+  //   if (jsCount > pythonCount || jsCount > cSharpCount)
+  //     answer = "JavaScript is your Language"
+  //   else if (pythonCount > jsCount || pythonCount > cSharpCount)
+  //     answer = "Python is your language"
+  //   else if (cSharpCount > jsCount || cSharpCount > pythonCount)
+  //     answer = "C# is your language"
+  //   else 
+  //     answer = "Try again with a little more thought, we didn't get enough specifics"
+  //   result = answer
+  // };
